c35d787ac25ea474a8f140abf8ff73d2f1de538d
Mohamed Ehab <medo.ehab2005@gmail.com>
2025-12-27 21:34:00 +0200

    Update config and cost.py

--- config and cost.py
+++ config and cost.py
@@ -2,10 +2,6 @@
 # Time unit: 1 period = 1 month
 # Inventory unit: single medical supply unit (e.g., box of masks)
 
-
-
-
-
 # Planning horizon
 T = 12  # 12 months (1 year planning)
 
@@ -25,10 +21,14 @@ MAX_STORAGE = 500    # Maximum warehouse capacity (units)
 
 #Cost functions 
 def normalOrder(n):
+    if n == 0:
+        return 0
     return C_ORDER_FIXED + C_UNIT * n
 
 
 def emergencyOrder(n):
+    if n == 0:
+        return 0
     return C_EMERGENCY_FIXED + C_EMERGENCY_UNIT * n
 
 def storage(n):
@@ -36,11 +36,12 @@ def storage(n):
 
 def OptimalCost(demand):
     dp = [[float('inf')] * (MAX_STORAGE + 1) for _ in range(T + 1)]
-    
     # Base case: at time T, no cost regardless of inventory
     for I in range(MAX_STORAGE + 1):
-        dp[T][I] = 0  # No cost at time 0
-        for t in range(T - 1, -1, -1):
+        dp[T][I] = 0  # No cost at time 12
+    # Fill dp table backwards
+    for t in range(T - 1, -1, -1):
+        for I in range(MAX_STORAGE + 1):  # current inventory
             for q in range(MAX_STORAGE - I + 1):  # feasible order quantities
                 available = I + q
                 shortage = max(0, demand[t] - available)
